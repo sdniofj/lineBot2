3cedbd52bcd698f8ecf2b75ee9488ff168a7e42a
sdniofj <56059140+sdniofj@users.noreply.github.com>
2019-10-02 12:05:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    if(userText==('อิหยังวะ'):
+    if (userText==('อิหยังวะ'):
     sendText(user,'หิวเมื่อไหร่ก็แวะมา7-11ตึ่ง')
-    if(userText== 'เหงาเหรอ'):
+    if (userText== 'เหงาเหรอ'):
     sendText(user,'เหงามากหาคนดูแล')
-    if(userText== 'ดูแลตัวเองไปก่อนนะ'):
+    if (userText== 'ดูแลตัวเองไปก่อนนะ'):
     sendText(user,'ถถถ.แม่คู้นนนน')
     if(userText == 'สวัสดี') :
     sendText(user,'เออดีด้วย')
